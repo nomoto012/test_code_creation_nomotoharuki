adf73f6330d35a3a91c313a940f493d38e1b7a6e
h-nomoto <h-nomoto@No383.nexis.local>
2026-09-24 16:48:34 +0900

    add:Case5.埜本明希  トップページアクセス、ログイン機能、ヘルプ画面遷移、よくある質問画面遷移機能の追加

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case05.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case05.java
@@ -3,6 +3,10 @@ package jp.co.sss.lms.ct.f02_faq;
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -11,7 +15,7 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.openqa.selenium.By;
-
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト よくある質問機能
@@ -21,6 +25,18 @@ import org.openqa.selenium.By;
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース05 キーワード検索 正常系")
 public class Case05 {
+
+	String url = "http://localhost:8080/lms";
+
+	String title = "ログイン | LMS";
+
+	String helptitle = "ヘルプ | LMS";
+
+	String questiontitle = "よくある質問 | LMS";
+
+	String id = "StudentAA02";
+
+	String pass = "StudentAA022";
 
 	/** 前処理 */
 	@BeforeAll
@@ -39,6 +55,12 @@ public class Case05 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		goTo(url);
+
+		assertEquals(title, webDriver.getTitle());
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
@@ -46,13 +68,40 @@ public class Case05 {
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
 		// TODO ここに追加
+		final WebElement loginid = webDriver.findElement(By.name("loginId"));
+		final WebElement password = webDriver.findElement(By.name("password"));
+		final WebElement loginclick = webDriver.findElement(By.className("btn-primary"));
+
+		loginid.clear();
+		password.clear();
+
+		loginid.sendKeys(id);
+		password.sendKeys(pass);
+
+		assertEquals("StudentAA02", id, "ログインIDが一致していません");
+		assertEquals("StudentAA022", pass, "passwordが一致していません");
+
+		loginclick.click();
+
+		getEvidence(new Object() {
+		});
 	}
-	
+
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
 		// TODO ここに追加
+		final WebElement functionclick = webDriver.findElement(By.className("dropdown-toggle"));
+		functionclick.click();
+
+		final WebElement helpclick = webDriver.findElement(By.linkText("ヘルプ"));
+		helpclick.click();
+
+		assertEquals(helptitle, webDriver.getTitle());
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
@@ -60,14 +109,29 @@ public class Case05 {
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
 		// TODO ここに追加
+		final WebElement questionclick = webDriver.findElement(By.linkText("よくある質問"));
+
+		questionclick.click();
+
+		Set<String> handles = webDriver.getWindowHandles();
+
+		List<String> list = new ArrayList<>(handles);
+
+		webDriver.switchTo().window(list.get(list.size() - 1));
+
+		assertEquals(questiontitle, webDriver.getTitle());
+
+		getEvidence(new Object() {
+		});
 	}
+
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 キーワード検索で該当キーワードを含む検索結果だけ表示")
 	void test05() {
 		// TODO ここに追加
 	}
-	
+
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 「クリア」ボタン押下で入力したキーワードを消去")
