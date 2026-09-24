193b60e4d51fb5651631f825f765a890a839516b
h-nomoto <h-nomoto@No383.nexis.local>
2026-09-24 13:04:15 +0900

    ケース3のコミット

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能①
@@ -18,6 +21,14 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース03 受講生 ログイン 正常系")
 public class Case03 {
+
+	String url = "http://localhost:8080/lms";
+
+	String title = "ログイン | LMS";
+
+	String id = "StudentAA02";
+
+	String pass = "StudentAA022";
 
 	/** 前処理 */
 	@BeforeAll
@@ -36,6 +47,12 @@ public class Case03 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		goTo(url);
+
+		assertEquals(title, webDriver.getTitle());
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
@@ -43,6 +60,23 @@ public class Case03 {
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
 		// TODO ここに追加
+		final WebElement loginid = webDriver.findElement(By.name("loginId"));
+		final WebElement password = webDriver.findElement(By.name("password"));
+		final WebElement loginclick = webDriver.findElement(By.className("btn-primary"));
+
+		loginid.clear();
+		password.clear();
+
+		loginid.sendKeys(id);
+		password.sendKeys(pass);
+
+		assertEquals("StudentAA02", id, "ログインIDが一致していません");
+		assertEquals("StudentAA022", pass, "passwordが一致していません");
+
+		loginclick.click();
+
+		getEvidence(new Object() {
+		});
 	}
 
 }
